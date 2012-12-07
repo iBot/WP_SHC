361052ce13a8e355aacb729877646c006002ab94
iBot <bla@bla.de>
2012-12-07 13:10:25 +0100

    linearlayout for lightlayout

--- MyController/gen/de/haw/shc/R.java
+++ MyController/gen/de/haw/shc/R.java
@@ -14,6 +14,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int button1=0x7f060004;
+        public static final int button2=0x7f060005;
         public static final int context_list=0x7f060000;
         public static final int control=0x7f060003;
         public static final int control_container=0x7f060001;
