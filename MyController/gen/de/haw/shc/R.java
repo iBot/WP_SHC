1966880539782b6fc2d71395b017603d1b71f40f
iBot <bla@bla.de>
2012-12-07 14:39:10 +0100

    create a usefull links doc

--- MyController/gen/de/haw/shc/R.java
+++ MyController/gen/de/haw/shc/R.java
@@ -14,13 +14,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int button1=0x7f060005;
+        public static final int button2=0x7f060006;
+        public static final int button3=0x7f060008;
+        public static final int button4=0x7f060009;
         public static final int context_list=0x7f060000;
         public static final int control=0x7f060003;
         public static final int control_container=0x7f060001;
         public static final int textView1=0x7f060002;
         public static final int textView2=0x7f060004;
-        public static final int textView3=0x7f060005;
-        public static final int textView4=0x7f060006;
+        public static final int textView3=0x7f060007;
+        public static final int textView4=0x7f06000a;
     }
     public static final class layout {
         public static final int activity_context_list=0x7f030000;
